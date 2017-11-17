414ad1ceca7589618e9bfbc82813cbb0bd5ae8b4
xuchichi <xuchichi@qh-speed.com>
2017-11-17 15:10:17 +0800

    yeah

---
+++ app/src/main/java/ui/activity/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.custom.cainiaowo.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
