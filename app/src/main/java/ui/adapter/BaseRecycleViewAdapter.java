6ef197779eee6b7cb3864ef1a12d883e9d21b29b
xuchichi <1516230950@qq.com>
2017-11-19 20:24:20 +0800

    Adadpter 封装

--- app/src/main/java/ui/adapter/BaseRecycleViewAdapter.java
+++ app/src/main/java/ui/adapter/BaseRecycleViewAdapter.java
@@ -2,13 +2,13 @@ package ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.util.List;
+
+import utils.OnItemClickListener;
 
 /**
  * Created by xuchichi on 2017/11/7.
@@ -19,6 +19,17 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
     private int LayoutId;
     private Context context;
     private LayoutInflater inflater;
+    public OnItemClickListener onItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View itemView,int position);
+    }
+    public void setOnitemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener=onItemClickListener;
+    }
+
+
+
 
     public BaseRecycleViewAdapter(List<T> mlist, int layoutId, Context context) {
         this.dataList = mlist;
@@ -27,21 +38,35 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
         this.inflater = LayoutInflater.from(context);
     }
 
-    @Override
-    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=inflater.inflate(LayoutId,parent,false);
-        return new BaseViewHolder(view);
+    public BaseRecycleViewAdapter(List<T> dataList, int layoutId, Context context, LayoutInflater inflater, OnItemClickListener onItemClickListener) {
+        this.dataList = dataList;
+        LayoutId = layoutId;
+        this.context = context;
+        this.inflater = inflater;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @Override
-    public void onBindViewHolder(BaseViewHolder holder, int position) {
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=inflater.inflate(LayoutId,null);
+        BaseViewHolder holder=new BaseViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder holder, final int position) {
         bindData(holder,dataList.get(position),position);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onItemClickListener.onItemClick(view,position);
+            }
+        });
     }
     protected abstract void bindData(BaseViewHolder holder, T data, int position);
     @Override
     public int getItemCount() {
         return dataList.size();
-
     }
 
     @Override
--- app/src/main/java/utils/OnItemClickListener.java
+++ app/src/main/java/utils/OnItemClickListener.java
@@ -7,5 +7,5 @@ import android.view.View;
  */
 
 public interface OnItemClickListener {
-    void onItemClick(View itemView,int position);
+    void onItemClick(View itemView);
 }
